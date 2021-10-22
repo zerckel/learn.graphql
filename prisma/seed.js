c966564be65cfda8cea9d0b7dd302eb27fd16330
Antoine Carbonnel <antoinecarbonnel@yahoo.fr>
2021-10-22 23:44:05 +0200

    add: final commit

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,92 +3,113 @@ const {PrismaClient, Prisma} = require("@prisma/client");
 const prisma = new PrismaClient()
 
 const userData = [{
-  id: 1,
   firstname: "Lilith",
   lastname: "Raddon",
   email: "lraddon0@1688.com",
   password: "j3cQqZUgxe6"
 }, {
-  id: 2,
   firstname: "Dallon",
   lastname: "Ertel",
   email: "dertel1@mozilla.com",
   password: "6EVNXiLC5VF7"
 }, {
-  id: 3,
   firstname: "Menard",
   lastname: "Wytchard",
   email: "mwytchard2@digg.com",
   password: "68F3WWF83Xt"
 }, {
-  id: 4,
   firstname: "Ulises",
   lastname: "Hampstead",
   email: "uhampstead3@telegraph.co.uk",
   password: "lCPGVkwznk2k"
 }, {
-  id: 5,
   firstname: "Cynde",
   lastname: "Matyas",
   email: "cmatyas4@sciencedirect.com",
   password: "84u5i5b"
 }]
+const commentData = [{
+  content: "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  postId: 1,
+  userId: 1
+}, {
+  content: "Fusce consequat. Nulla nisl. Nunc nisl.",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  postId: 2,
+  userId: 2
+}, {
+  content: "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  postId: 2,
+  userId: 3
+}, {
+  content: "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  postId: 3,
+  userId: 2
+}, {
+  content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  postId: 4,
+  userId: 5
+}]
 const postData = [{
-  id: "1",
   title: "in ante vestibulum ante ipsum primis in faucibus orci luctus et",
   content: "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
-  createdAt: "12/12/2020",
-  updatedAt: "10/17/2021",
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  userId: 1
 }, {
-  id: "2",
   title: "dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien placerat",
   content: "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
-  createdAt: "12/30/2020",
-  updatedAt: "12/8/2020"
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  userId: 2
 }, {
-  id: "3",
   title: "dui vel nisl duis ac nibh fusce lacus purus aliquet at",
   content: "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
-  createdAt: "4/28/2021",
-  updatedAt: "3/17/2021"
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  userId: 2
 }, {
-  id: "4",
   title: "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer",
   content: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
-  createdAt: "12/8/2020",
-  updatedAt: "5/18/2021"
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  userId: 3
 }, {
-  id: "5",
   title: "mi pede malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim lorem",
   content: "In congue. Etiam justo. Etiam pretium iaculis justo.",
-  createdAt: "6/29/2021",
-  updatedAt: "10/12/2021"
+  createdAt: new Date("12/12/2021"),
+  updatedAt: new Date("12/12/2021"),
+  userId: 5
 }]
 
-
 async function main() {
-  console.log(`start seeding ...`)
   for (const u of userData) {
     const user = await prisma.user.create({
       data: u
     })
-    console.log(`Create user with id: ${user.id}`)
-  }
-  for (const c of commentData) {
-    const user = await prisma.comment.create({
-      data: c
-    })
   }
   for (const p of postData) {
-    const comment = await prisma.post.create({
+    const post = await prisma.post.create({
       data: p
     })
   }
-
-  console.log(`Seeding finished`)
+  for (const c of commentData) {
+    const comment = await prisma.comment.create({
+      data: c
+    })
+  }
 }
 
-main.catch((e) => {
+main().catch((e) => {
   console.error(e)
   process.exit(1)
 }).finally(async () => {
--- src/index.js
+++ src/index.js
@@ -1,75 +1,194 @@
 const {ApolloServer, gql} = require('apollo-server');
+const { PrismaCLient } = require('@prisma/client');
+
+const prisma = new PrismaCLient()
 
 const typeDefs = gql`
+    scalar DateTime
     # This "Book" type defines the queryable fields for every book in our data source.
-    type User {
-        id: Int
-        pseudo: String
+    type Post{
+        id: ID
+        title: String
+        content: String
+        userId: Int
+        createdAt: DateTime
+        updatedAt: DateTime
     }
 
-    type Post {
+    type Comment{
         id: Int
-        author: User
         content: String
-        comments: [Post]
+        userId: Int
+        postId: Int
+        createdAt: DateTime
+        updatedAt: DateTime
+    }
+
+    type User{
+        id: ID
+        firstname: String
+        lastname: String
+        password: String
+        email: String
+        comments : [Comment]
+        posts: [Post]
     }
 
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each. In this
     # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
-        users: [User]
-        user(id: Int!): User
-        posts: [Post]
-        post(id: Int!): Post
+        posts : [Post]!
+        post(id: ID!): Post
+        comment(id: ID!):Comment
+        getCommentFromPost(id:ID!):Post
+    }
+    input UserInput{
+        firstname: String
+        lastname: String
+        password: String
+        email: String
+    }
+    input PostInput{
+        title: String
+        content: String
+        userId: Int
+    }
+    input UpdatePostInput{
+        title: String
+        content: String
+    }
+    input UpdateUserInput{
+        firstname: String
+        lastname: String
+        password: String
+        email: String
+    }
+    input CommentInput{
+        content: String
+    }
+    input InsertCommentInput{
+        content: String
+        userId: Int
+    }
+    type Mutation {
+        updatePost(input: UpdatePostInput, id: ID!): Post
+        deletePost(id: ID!): Boolean
+        updateComment(input: CommentInput, id: ID!): Comment
+        insertUser(input: UserInput): User
+        insertPost(input: PostInput): Post
+        deleteComment(id: ID!): Boolean
+        updateUser(input: UpdateUserInput, id: ID!): User
+        insertComment(input: InsertCommentInput, id: ID!): Comment
     }
 `;
 
-const users = [
-  {
-    id: 1,
-    pseudo: 'zerckel',
-  },
-  {
-    id: 2,
-    pseudo: 'tiboInShape',
-  },
-];
 
-const posts = [
-  {
-    id: 1,
-    author: users[1],
-    content: 'wsh la street',
-    comments: []
-  },
-  {
-    id: 2,
-    author: users[0],
-    content: 'wsh la street 1',
-    comments: []
-  },
-  {
-    id: 1,
-    author: users[1],
-    content: 'wsh la street 2',
-    comments: []
-  },
-];
 
 const resolvers = {
   Query: {
-    users: () => users,
-    user(parent, args) {
-      return users.find(user => user.id === args.id)
+    post: (parent, args, context, info) => {
+      return context.prisma.post.findUnique({
+        where: {
+          id: parseInt(args.id)
+        }
+      })
     },
-    posts: () => posts,
-    post(parent, args) {
-      return posts.find(post => post.id === args.id)
+    posts: (parent, args, context, info) => {
+      return context.prisma.post.findMany()
+    },
+    getCommentFromPost: (parent, args, context, info) => {
+      const posts = context.prisma.post.findUnique({
+        where: {
+          id: parseInt(args.id)
+        }
+      })
+      return posts.data.posts.comments
+    },
+    comment: (parent, args, context, info) => {
+      return context.prisma.comment.findUnique({
+        where: {
+          id: parseInt(args.id)
+        }
+      })
     }
   },
-};
+  Mutation: {
+    insertUser: (parent, args, context, info) => {
+      return context.prisma.user.create({
+        data: args.input,
+      })
+    },
+    insertPost: (parent, args, context, info) => {
+      return context.prisma.post.create({
+        data:{
+          title: args.input.title,
+          content:  args.input.content,
+          userId:  parseInt(args.input.userId),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }
+      })
+    },
+    updatePost: (parent, args, context, info) => {
+      return context.prisma.post.update({
+        where: {
+          id: parseInt(args.id),
+        },
+        data: {
+          title: args.input.title,
+          content: args.input.content,
+          updatedAt: new Date()
+        }
+      })
+    },
+    deletePost: async (parent, args, context, info) => {
+      try {
+        await context.prisma.post.delete({
+          where: {
+            id: parseInt(args.id)
+          }
+        })
+        return true
+      } catch (e) {
+        return false
+      }
 
+    },
+    deleteComment: async (parent, args, context, info) => {
+      try {
+        await context.prisma.comment.delete({
+          where: {
+            id: parseInt(args.id)
+          }
+        })
+        return true
+      } catch (e) {
+        return false
+      }
+
+    },
+    updateComment: (parent, args, context, info) => {
+      return context.prisma.comment.update({
+        where: {
+          id: parseInt(args.id),
+        },
+        data: args.input,
+      })
+    },
+    insertComment: (parent, args, context, info) => {
+      return context.prisma.comment.create({
+        data: {
+          content: args.input.content,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          postId: parseInt(args.id),
+          userId: parseInt(args.input.userId),
+        }
+      })
+    }
+  }
+};
 const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url}) => {
